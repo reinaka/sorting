8d89715d3d6b454cb570fb15318a90f2db55194a
Julia Bednaia <juliabednaia@gmail.com>
2023-10-13 12:07:15 +0300

    insertion sort

---
+++ insertion_sort.js
@@ -0,0 +1,24 @@
+/*
+СОРТИРОВКА ВСТАВКАМИ
+
+Временная сложность: O(n^2)
+Объём памяти, помимо места под хранение массива: O(1)
+Устойчивость: сортировка устойчивая
+
+*/
+
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        let item_to_insert = array[i];
+        let j = i;
+        while (j > 0 && item_to_insert < array[j - 1]) {
+        array[j] = array[j - 1];
+        j--;
+    }
+        array[j] = item_to_insert;
+        console.log(`step ${i}, sorted ${i + 1} elements: ${array.slice(0, i + 1).join(" ")}`);
+    }
+}
+
+let array = [5, 2, 4, 6, 1, 3];
+insertionSort(array); 
